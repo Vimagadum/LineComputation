bedf705f2fd5c6a06d4847868abecdc0c1835d15
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-01-15 14:50:22 +0530

    [Vinayak] adding Line Computing program

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using CalculateLength;
+
+Console.WriteLine("Hello, World!");
+Line_Equal eql = new Line_Equal();
+eql.equal();
+
+
+
